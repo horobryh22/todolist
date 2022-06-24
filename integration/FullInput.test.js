9d48b91e2b31e3841079d8e2d2d9b218a06e4bb0
Ilya Khorobrykh <horobryh17@gmail.com>
2022-06-24 18:22:23 +0300

    Commit before next lesson

--- integration/FullInput.test.js
+++ integration/FullInput.test.js
@@ -1,10 +1,21 @@
 describe('FullInput', () => {
     it('base example, visually looks correct', async () => {
         // APIs from jest-puppeteer
-        await page.goto('http://localhost:9009/iframe.html?id=todolist-fillinput--fill-input-base-example&viewMode=story')
+        await page.goto('http://localhost:9009/iframe.html?id=todolist-components-fillinput--fill-input-base-example&viewMode=story')
         const image = await page.screenshot()
 
         // API from jest-image-snapshot
         expect(image).toMatchImageSnapshot()
     })
 })
+
+// describe('Task', () => {
+//     it('base example, visually looks correct', async () => {
+//         // APIs from jest-puppeteer
+//         await page.goto('http://localhost:9009/iframe.html?id=todolist-components-task--task-base-example&viewMode=story')
+//         const image = await page.screenshot()
+//
+//         // API from jest-image-snapshot
+//         expect(image).toMatchImageSnapshot()
+//     })
+// })
