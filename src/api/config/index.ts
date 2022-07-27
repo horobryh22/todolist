2a59a70ad66da665916aa286b326876d5896a229
Ilya Khorobrykh <horobryh17@gmail.com>
2022-07-27 14:43:38 +0300

    I resolved conflict with eslint-plugin - react

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '9f5fc3ee-790f-410c-959d-2faeddcc9f8e'
-    }
-})
+        'API-KEY': '9f5fc3ee-790f-410c-959d-2faeddcc9f8e',
+    },
+});
